a206af188c26c3b2779cad173763356158d4ff0b
ParaBeber <jberner30@gmail.com>
2025-05-06 12:00:58 +0200

    Update ContactSection.tsx

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -121,11 +121,11 @@ export const ContactSection = () => {
                   <div className="space-y-2">
                     <p className="flex items-center gap-2 text-gray-700">
                       <MapPin size={18} className="text-blue-600" />
-                      1744 Avenue de l'Europe, 34170 Castelnau-le-Lez
+                      155 Rue Alphonse Beau de Rochas, 34170 Castelnau-le-Lez
                     </p>
                     <p className="flex items-center gap-2 text-gray-700">
                       <Phone size={18} className="text-blue-600" />
-                      04 67 72 50 20
+                      07 67 27 41 87
                     </p>
                   </div>
                 </div>
@@ -134,18 +134,18 @@ export const ContactSection = () => {
                   <div className="space-y-2">
                     <p className="flex items-center gap-2 text-gray-700">
                       <MapPin size={18} className="text-blue-600" />
-                      1 rue des Chevaliers de Malte, 34070 Montpellier
+                      769 Av. de la Mer-Raymond Dugrand, 34000 Montpellier
                     </p>
                     <p className="flex items-center gap-2 text-gray-700">
                       <Phone size={18} className="text-blue-600" />
-                      04 67 34 43 00
+                      07 49 32 41 38
                     </p>
                   </div>
                 </div>
                 <div>
                   <p className="flex items-center gap-2 text-gray-700">
                     <Mail size={18} className="text-blue-600" />
-                    contact@aquabike-center.fr
+                    contact@aquabikecenter.club
                   </p>
                 </div>
               </div>
